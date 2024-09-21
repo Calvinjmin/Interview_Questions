0ff189274488719e60e6011b9f0665ddc4b84a7c
Calvin Min <32576363+Calvinjmin@users.noreply.github.com>
2024-09-21 15:39:30 -0400

    Reverse Integer

---
+++ C++/Math/reverse-integer-7.cpp
@@ -0,0 +1,19 @@
+// Basic Math Manipulation - [Leetcode 7](https://leetcode.com/problems/reverse-integer/description/?)
+
+#include <climits>
+using namespace std;
+
+
+int reverse(int x) {
+    int rev = 0;
+    while ( x != 0 ) {
+        int pop = x % 10;
+        x /= 10;
+        if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && pop > 7))
+            return 0;
+        if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && pop < -8))
+            return 0;
+        rev = (10 * rev) + pop;
+    }
+    return rev;
+}
